a4c1829f525f657a6e5f3a3ae647a1e38b7426fa
Tima Mehro <f.f.mehro@gmail.com>
2025-06-08 18:04:57 +1000

    Remove extra config from next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  // add redirect
-  redirects: async () => {
-    return [
-      {
-        source: "/",
-        destination: "/",
-        permanent: true,
-      },
-    ]
-  },
-
-  //  TODO: add BundleAnalyzer
+  reactStrictMode: true,
 }
 
 export default nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
         <div className="text-2xl">Coming Soon</div>
-        Wegit re working on something awesome. Stay tuned!
+        We are working on something awesome. Stay tuned!
       </div>
     </div>
   )
