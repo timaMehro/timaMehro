cb9422a71ae168476a0debe57741abf0aa622cdc
Tima Mehro <f.f.mehro@gmail.com>
2025-08-08 15:05:04 +1000

    Fix build issue

--- src/app/react/page.tsx
+++ src/app/react/page.tsx
@@ -1,14 +1,7 @@
 "use client"
-import { useSearchParams } from "next/navigation"
 import ReactPage from "./rect"
 
 export default function ReactArticle() {
-  const articleId = useSearchParams().get("articleId")
-
-  console.log("articleId", articleId)
-  if (!articleId) {
-    return <ReactPage />
-  }
-
-  return <div>React Article: {articleId}</div>
+  // console.log("articleId", articleId)
+  return <ReactPage />
 }
