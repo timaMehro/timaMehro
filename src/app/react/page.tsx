8a9f5ef7272b44f8985371b2724ddc7ace9cf48b
Tima Mehro <f.f.mehro@gmail.com>
2025-08-08 15:18:26 +1000

    Add `<nav />` component to react page

--- src/app/react/page.tsx
+++ src/app/react/page.tsx
@@ -1,7 +1,15 @@
 "use client"
+import Nav from "../Nav"
 import ReactPage from "./rect"
 
 export default function ReactArticle() {
-  // console.log("articleId", articleId)
-  return <ReactPage />
+  return (
+    <main
+      className="min-h-screen  bg-gradient-to-tr from-gray-900 via-indigo-900 to-black  md:w-full
+       text-white font-[family-name:var(--font-geist-sans)] md:px-30  p-3 py-1"
+    >
+      <Nav />
+      <ReactPage />
+    </main>
+  )
 }
